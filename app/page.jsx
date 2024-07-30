d1ad6c5c7ef630e8152a6d6a2c3e9b03e9257d44
iamnards <nardskie19@gmail.com>
2024-07-30 11:59:47 +0800

    fix attempt 4

--- app/page.jsx
+++ app/page.jsx
@@ -10,9 +10,9 @@ import { TypeAnimation } from "react-type-animation";
 
 export default function Home() {
   return (
-    <main className="h-screen flex z-10">
+    <main className="h-screen flex z-10 bg-[#181818]">
       <div className="h-screen w-full lg:w-1/2 place-content-center px-0 md:px-10 lg:px-20 z-10">
-        <h2 className="text-white text-3xl lg:text-5xl font-extrabold pt-30 lg:pt-0 py-2 text-center lg:text-left">
+        <h2 className="text-white text-3xl lg:text-5xl font-extrabold place-content-center lg:pt-0 py-2 text-center lg:text-left">
           Nard Sarroca
         </h2>
         <p className="text-paragraph text-lg lg:text-2xl py-2 text-center lg:text-left">
